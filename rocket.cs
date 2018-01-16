f626cde6e063e03c7fdfa0fddd9689b179741f1f
jaysonq <jaysonquitain18@gmail.com>
2018-01-16 17:20:37 +0800

    Update rocket.cs

--- rocket.cs
+++ rocket.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,7 +7,7 @@ public class rocket : MonoBehaviour {
     [SerializeField] Vector3 initialpos;
     [SerializeField] GameObject Rocket;
     GameObject touchdown;
-    bool isPlaying;
+    //bool isPlaying;
 
 	// Use this for initialization
 	void Start () {
@@ -15,40 +15,20 @@ public class rocket : MonoBehaviour {
         initialpos = new Vector3(-0.1798952f, 4.512645f, -1.240017f);
         Rocket = GameObject.Find("rocketship");
         touchdown = GameObject.Find("touchdown");
-        isPlaying = true;
+
 	}
-    void EnableRigidcody()
-    {
-        _rigidbody.isKinematic = false;
-        _rigidbody.detectCollisions = true;
-    }
-    void DisableRigigbody()
-    {
-        _rigidbody.isKinematic = true;
-        _rigidbody.detectCollisions = false;
-
-
-    }
 
     // Update is called once per frame
     void Update () {
-        if (isPlaying == true)
-        {
+     
             ProcessInput();
-        }
-        else {
-            Rocket.transform.position = initialpos;
-            Rocket.transform.rotation = Quaternion.identity;
-            touchdown.GetComponent<Renderer>().material.color = Color.red;
-            EnableRigidcody();
 
-        }
 	}
     public void ProcessInput() {
         if (Input.GetKey(KeyCode.Space))
         {
             print("space");
-            _rigidbody.AddRelativeForce(Vector3.up);
+            _rigidbody.AddRelativeForce(Vector3.up*2);
         }
         if (Input.GetKey(KeyCode.A)) {
             transform.Rotate(Vector3.forward*2);
@@ -58,23 +38,29 @@ public class rocket : MonoBehaviour {
             transform.Rotate(-Vector3.forward*2);
         }
 
+
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name == "obs1" || collision.gameObject.name == "obs2" ||
-            collision.gameObject.name == "wall left" || collision.gameObject.name == "wall right" ||
-            collision.gameObject.name == "ceilling" || collision.gameObject.name == "terrain") {
+        if (collision.gameObject.name == "obs1" || collision.gameObject.name == "obs2" 
+            || collision.gameObject.name == "obs3" || collision.gameObject.name == "wall left" 
+            || collision.gameObject.name == "wall right"|| collision.gameObject.name == "ceilling" 
+            || collision.gameObject.name == "terrain") {
+
             Rocket.transform.position = initialpos;
             Rocket.transform.rotation = Quaternion.identity;
             _rigidbody.angularVelocity = new Vector3(0f,0f,0f);
-            touchdown.GetComponent<Renderer>().material.color = Color.red;
+            
 
         }
-        if (collision.gameObject.name == "touchdown") {
+        if (collision.gameObject.name == "touchdown")
+        {
             touchdown.GetComponent<Renderer>().material.color = Color.green;
-            DisableRigigbody();
         }
     }
-
+    private void OnCollisionExit(Collision collision)
+    {
+        touchdown.GetComponent<Renderer>().material.color = Color.red;
+    }
 
 }
